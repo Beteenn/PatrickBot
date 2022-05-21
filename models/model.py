7d60a7460cd3ae32ed7818410f81c41a11b43273
Fabio Vilela <fabiovilela123@gmail.com>
2022-05-21 01:10:10 -0300

    add model

---
+++ models/model.py
@@ -0,0 +1,12 @@
+class MultiItems:
+
+    def __init__(self):
+        self.message = ""
+        self.items = []
+
+    def __init__(self, message, items):
+        self.message = message
+        self.items = items
+
+    def __str__(self):
+        return f"message:{self.message} items:{self.items}"
